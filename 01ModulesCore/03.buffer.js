b7fd367fb5a7ef32f27fc5972e91c494dc9be006
SebastianReyesL <sebastian.reyes2002@hotmail.com>
2020-04-05 00:56:25 -0500

    Archivo de buffer.js finalizado con un poco de teoria

--- 01ModulesCore/03.buffer.js
+++ 01ModulesCore/03.buffer.js
@@ -1,3 +1,16 @@
+/*
+Buffers
+    - Una tira de bytes (datos binarios)
+    - Similar a un array de enteros
+    - Tamaño fijo
+    - Manipular datos directamente
+        · Sockets
+        · Streams
+        · Implementar protocolos complejos
+        · Manipulación de ficheros/imágenes
+        · Criptografía
+*/
+
 'use strict'
 
 let buffer = new Buffer(100),
@@ -7,5 +20,14 @@ let buffer = new Buffer(100),
 buffer.write('abcd', 0, 4, 'ascii')
 console.log(
     buffer,
-    buffer.toString('ascii')
+    buffer.toString('ascii'),
+    str,
+    str.length + 'caracteres',
+    Buffer.byteLength(str, 'utf8') + 'bytes',
+    buffer2.length
 )
+
+for (let i = 0; i < buffer2.length; i++) {
+    buffer2[i] = i + 97
+}
+console.log(buffer2.toString('ascii'))
