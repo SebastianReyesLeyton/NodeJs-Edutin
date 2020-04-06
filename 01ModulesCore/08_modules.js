9c43c05294384ca35ed2e10dc3728a516c466801
SebastianReyesL <sebastian.reyes2002@hotmail.com>
2020-04-05 23:18:55 -0500

    Reloj digital ES5

--- 01ModulesCore/08_modules.js
+++ 01ModulesCore/08_modules.js
@@ -1,7 +1,13 @@
 'use strict'
 
-const myData = require('./my_data')
+const myData = require('./my_data'),
+    Clock = require('./clock_es5'),
+    cucu = new Clock()
 
 console.log(
     `Nombre: ${myData.name}\nEmail: ${myData.email}`
 )
+
+cucu.on('tictak', function() {
+    cucu.theTime()
+})
