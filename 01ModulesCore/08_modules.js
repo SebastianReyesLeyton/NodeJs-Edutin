5b2422264d40b9575aec823d32204a70eadd2a38
SebastianReyesL <sebastian.reyes2002@hotmail.com>
2020-04-05 22:47:36 -0500

    Exportar modulos y requerirlos en otros archivos adicionales

---
+++ 01ModulesCore/08_modules.js
@@ -0,0 +1,7 @@
+'use strict'
+
+const myData = require('./my_data')
+
+console.log(
+    `Nombre: ${myData.name}\nEmail: ${myData.email}`
+)
---
+++ 01ModulesCore/my_data.js
@@ -0,0 +1,8 @@
+'use strict'
+
+let name = 'Jonathan',
+    email = 'jonmircha@bextlan.com',
+    _phone = '5215518388261'
+
+module.exports.name = name
+module.exports.email = email
